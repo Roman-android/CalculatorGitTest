2df58e6b55c934a1d135f7ff20c66fbecd10d333
Roman Malkov <roman-li-83@yandex.ru>
2018-05-16 21:29:24 +0500

    First commit. Application created. Primary appearance.

---
+++ app/src/main/java/ru/roman/calculatorgittest/MainActivity.java
@@ -0,0 +1,41 @@
+package ru.roman.calculatorgittest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText input_number_1, input_number_2;
+    Button show_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        input_number_1 = findViewById(R.id.num_1);
+        input_number_1 = findViewById(R.id.num_2);
+        show_result = findViewById(R.id.btn_result);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        
+    }
+
+
+}
