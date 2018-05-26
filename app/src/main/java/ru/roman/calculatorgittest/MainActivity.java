842b49a70262d31bb9e485fe9b1530660ac68ea8
Roman Malkov <roman-li-83@yandex.ru>
2018-05-26 19:49:20 +0500

    Add items in ListView (Connection in MainActivity)

--- app/src/main/java/ru/roman/calculatorgittest/MainActivity.java
+++ app/src/main/java/ru/roman/calculatorgittest/MainActivity.java
@@ -6,10 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import ru.roman.calculatorgittest.fragments.OutputListFragment;
 import ru.roman.calculatorgittest.utils.Calculation;
@@ -23,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     Button show_result;
     TextView text_result;
 
+    ListView listView;
+    ArrayList <String> results;
+    public ArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +39,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         calculation = new Calculation();
-        outputListFragment = new OutputListFragment();
+        //outputListFragment = new OutputListFragment();
+
+        results = new ArrayList<>();
+        listView = findViewById(R.id.dynamic_list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, results);
+        results.add("456");
+        listView.setAdapter(adapter);
 
         input_number_1 = findViewById(R.id.num_1);
         input_number_2 = findViewById(R.id.num_2);
@@ -53,17 +67,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View view) {
 
-
         String num_first = input_number_1.getText().toString();
         String num_second = input_number_2.getText().toString();
 
-        Toast.makeText(this, num_first, Toast.LENGTH_SHORT).show();
-        String summ_of_num = calculation.workCalculation(num_first,num_second);
-
-        text_result.setText(summ_of_num);
-
-        outputListFragment.results.add(summ_of_num);
-        outputListFragment.adapter.notifyDataSetChanged();
-
+        String sum_of_num = calculation.workCalculation(num_first,num_second);
+        Toast.makeText(this, sum_of_num, Toast.LENGTH_SHORT).show();
+        text_result.setText(sum_of_num);
+        //outputListFragment.updateList(sum_of_num);
+        results.add(sum_of_num);
+        adapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/ru/roman/calculatorgittest/fragments/OutputListFragment.java
+++ app/src/main/java/ru/roman/calculatorgittest/fragments/OutputListFragment.java
@@ -1,25 +1,84 @@
 package ru.roman.calculatorgittest.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class OutputListFragment extends ListFragment {
+import ru.roman.calculatorgittest.R;
+
+public class OutputListFragment extends Fragment {
 
     final String[] catNames = new String[]{"Рыжик", "Барсик", "Мурзик",
             "Мурка", "Васька", "Томасина", "Кристина", "Пушок", "Дымка"};
 
-    public ArrayList <String> results = new ArrayList<>();
+    public ArrayList<String> results;
     public ArrayAdapter adapter;
+    public String test;
+    private final String LOG_OUTPUT = "log_output_list";
+    ListView listView;
+
+    public OutputListFragment() {
+        test = "7896";
+    }
+
+    /*public static OutputListFragment newInstance(Context context) {
+        OutputListFragment outputListFragment = new OutputListFragment();
+        Bundle args = new Bundle();
+        args.putParcelable("context", context);
+        outputListFragment.setArguments(args);
+        return outputListFragment;
+    }*/
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, results);
-        setListAdapter(adapter);
+        Log.d(LOG_OUTPUT, String.valueOf("result = "+getContext()));
+        results = new ArrayList<>();
+        results.add("456");
     }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.list_output, container, false);
+        listView = view.findViewById(R.id.dynamic_list);
+
+        listView.setAdapter(adapter);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Log.d("Log","result = "+results.get(0));
+    }
+
+    public void updateList(String item) {
+        Log.d(LOG_OUTPUT, "result = " + item);
+
+        /*results.add(item);
+        if (adapter != null){
+            adapter.notifyDataSetChanged();
+        }else {
+            Log.d("Log","adapter == null, "+item);
+        }*/
+
+    }
+
+    ;
 }
